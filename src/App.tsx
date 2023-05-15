531c126b32efd7daf8bf997f48d2d0a971409cdb
mware <melvin.ware@advisorsexcel.com>
2023-05-15 13:44:46 -0500

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,41 @@
+type Song = {
+  createdBy: string;
+  createAt: string;
+  updatedBy: string;
+  updatedAt: string;
+  artist: string;
+  title: string;
+  length: string;
+};
+
+const songs: Song[] = [
+  {
+    createdBy: "admin@email.com",
+    createAt: "2021-01-01",
+    updatedAt: "2021-01-01",
+    updatedBy: "admin@email.com",
+    artist: "Pink Floyd",
+    title: "Time",
+    length: "6:53",
+  },
+  {
+    createdBy: "admin@email.com",
+    createAt: "2021-01-01",
+    updatedAt: "2021-01-01",
+    updatedBy: "admin@email.com",
+    artist: "The Beatles",
+    title: "Here Comes the Sun",
+    length: "3:05",
+  },
+];
+
 export function App() {
-  return <h1>Songs</h1>;
+  return (
+    <>
+      <h1>Songs</h1>
+      {songs.map((song) => {
+        return <div>{song.artist}</div>;
+      })}
+    </>
+  );
 }
